79771df91ca25e5a96407aa0f3c87abefe3c35d0
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-10-03 17:31:09 +0300

    style: format code with Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {HashRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../styles.scss';
-import Header from '../components/Header';
+import React from "react";
+import "../styles.scss";
+import Header from "../components/Header";
 
 function Home() {
   return (
