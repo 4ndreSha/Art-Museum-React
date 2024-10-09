b1f2250faf0e495f6cc6111bb9fa23c528a8da93
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-10-09 22:45:11 +0300

    feat: favorites page logic

--- src/components/BookmarkButton/index.tsx
+++ src/components/BookmarkButton/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { SessionStorageContext, useSessionStorageContext } from "@/utils/SessionStorage";
 
 import "@components/BookmarkButton/styles.scss";
 
-const BookmarkButton = () => {
-  return <button className="bookmarkButton" />;
+interface BookmarkButtonProps {
+  id: number;
+}
+
+const BookmarkButton: React.FC<BookmarkButtonProps> = ({ id }) => {
+  const sessionStorageContext = useSessionStorageContext();
+  const [isSaved, setIsSaved] = useState(sessionStorageContext.includes(id));
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    setIsSaved(!isSaved);
+    if (isSaved) {
+      sessionStorageContext.remove(id);
+    } else {
+      sessionStorageContext.add(id);
+    }
+  };
+
+  return <button className={`bookmark__button${isSaved ? "-saved" : ""}`} onClick={handleClick} />;
 };
 
 export default BookmarkButton;
