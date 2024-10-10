e9aae60d3983d2bbb4f058379580a71c30d058fe
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-10-10 03:32:26 +0300

    feat: burger menu
    3 hours of searchin for a mistake in animation. Solved by removing "display: none;".

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import logo from "@assets/museum-logo-dark.svg";
 import home from "@assets/home.svg";
@@ -9,6 +8,22 @@ import "@components/Header/styles.scss";
 
 const Header = () => {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const closeMenu = (e: MouseEvent) => {
+      if (isOpen && !document.querySelector(".header__nav").contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [isOpen]);
 
   return (
     <header className="header">
@@ -16,16 +31,25 @@ const Header = () => {
         <img src={logo} alt="Website logo" />
       </NavLink>
       <nav className="header__nav">
-        {location.pathname !== "/" && (
-          <NavLink to="/" className="header__nav-link">
-            <img src={home} alt="Home icon" />
-            <span>Home</span>
+        <div className="burger-menu" onClick={toggleMenu}>
+          <div className={`burger-menu__btn ${isOpen ? "open" : ""}`}>
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+        <div className={`nav-links ${isOpen ? "open" : ""}`}>
+          {location.pathname !== "/" && (
+            <NavLink to="/" className="header__nav-link" onClick={toggleMenu}>
+              <img src={home} alt="Home icon" />
+              <span>Home</span>
+            </NavLink>
+          )}
+          <NavLink to="/favorites" className="header__nav-link" onClick={toggleMenu}>
+            <img src={bookmark} alt="Bookmark icon" />
+            <span>Your favorites</span>
           </NavLink>
-        )}
-        <NavLink to="/favorites" className="header__nav-link">
-          <img src={bookmark} alt="Bookmark icon" />
-          <span>Your favorites</span>
-        </NavLink>
+        </div>
       </nav>
     </header>
   );
